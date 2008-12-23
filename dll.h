63f8bec5d34ef41fa419d4f7231463f705257b60
Kai-Chieh Ku <kjackie@gmail.com>
2008-12-23 12:18:14 +0000

    sync
    git-svn-id: https://kushellext.svn.sourceforge.net/svnroot/kushellext/trunk@4 ec8542c1-60f6-4486-b162-5f8329f6c6f8

--- dll.h
+++ dll.h
@@ -3,16 +3,16 @@
 
 #if defined(DLL_GETPROCADDR)
 	#define DLL_H_GETPROCADDR
-	#define DECLARE_DLL_PROC(name, dll, ret, call, ...) \
+	#define DECLARE_DLL_PROC(name, dll, ret, call, args) \
 		name = (name##Proc) GetProcAddress(GetModuleHandle(_T(dll)), #name);
 #elif defined(DLL_DEFINE)
 	#define DLL_H_DEFINE
-	#define DECLARE_DLL_PROC(name, dll, ret, call, ...) \
+	#define DECLARE_DLL_PROC(name, dll, ret, call, args) \
 		name##Proc name = NULL;
 #else
 	#define DLL_H_DECLARE
-	#define DECLARE_DLL_PROC(name, dll, ret, call, ...) \
-		typedef ret (call *name##Proc)(__VA_ARGS__); \
+	#define DECLARE_DLL_PROC(name, dll, ret, call, args) \
+		typedef ret (call *name##Proc) args; \
 		extern name##Proc name;
 #endif
 
@@ -24,11 +24,11 @@ namespace dll {
 
 
 
-DECLARE_DLL_PROC(CreateSymbolicLinkW, "kernel32", BOOL, APIENTRY, LPCWSTR lpSymlinkFileName, LPCWSTR lpTargetFileName, DWORD dwFlags)
+DECLARE_DLL_PROC(CreateSymbolicLinkW, "kernel32", BOOL, APIENTRY, (LPCWSTR lpSymlinkFileName, LPCWSTR lpTargetFileName, DWORD dwFlags))
 #ifndef _WIN64
-DECLARE_DLL_PROC(IsWow64Process, "kernel32", BOOL, WINAPI, HANDLE hProcess, PBOOL Wow64Process)
-DECLARE_DLL_PROC(Wow64DisableWow64FsRedirection, "kernel32", BOOL, WINAPI, PVOID* OldValue)
-DECLARE_DLL_PROC(Wow64RevertWow64FsRedirection, "kernel32", BOOL, WINAPI, PVOID OldValue)
+DECLARE_DLL_PROC(IsWow64Process, "kernel32", BOOL, WINAPI, (HANDLE hProcess, PBOOL Wow64Process))
+DECLARE_DLL_PROC(Wow64DisableWow64FsRedirection, "kernel32", BOOL, WINAPI, (PVOID* OldValue))
+DECLARE_DLL_PROC(Wow64RevertWow64FsRedirection, "kernel32", BOOL, WINAPI, (PVOID OldValue))
 #endif
 
 
